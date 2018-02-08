f4b97090b2ac827c5f33243ff642a6ad08ddbeee
Thomas Ritter <s9thritt@stud.uni-saarland.de>
2018-02-08 10:28:43 +0100

    moved SoftmaxClickMouse to envs.py and implemented SoftmaxClickMouseAndSubmit

--- universe-starter-agent/envs.py
+++ universe-starter-agent/envs.py
@@ -2,7 +2,8 @@ import cv2
 from gym.spaces.box import Box
 import numpy as np
 import gym
-from gym import spaces
+#from gym import spaces
+from universe import spaces
 import logging
 import universe
 from universe import vectorized
@@ -10,7 +11,7 @@ from universe.wrappers import BlockingReset, GymCoreAction, EpisodeID, Unvectori
 from universe import spaces as vnc_spaces
 from universe.spaces.vnc_event import keycode
 import time
-from universe.wrappers.experimental import SoftmaxClickMouse
+#from universe.wrappers.experimental import SoftmaxClickMouse
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 universe.configure_logging()
@@ -88,7 +89,7 @@ def create_miniwob_env(env_id, client_id, remotes, **_):
     env = BlockingReset(env)
 
     env = CropScreen(env, 160, 160, 125, 10)
-    if (env_id == 'wob.mini.ClickTest-v0') or (env_id == 'wob.mini.NumberCheckboxes-v0') or (env_id == 'wob.mini.ChaseCircle-v0'):
+    if (env_id == 'wob.mini.ClickTest-v0') or (env_id == 'wob.mini.NumberCheckboxes-v0') or (env_id == 'wob.mini.ChaseCircle-v0') or (env_id == 'wob.mini.BisectAngle-v0') or (env_id == 'wob.mini.FindMidpoint-v0') or (env_id == 'wob.mini.CircleCenter-v0'):
         obs_height = 80
         obs_width = 80
     else:
@@ -100,6 +101,8 @@ def create_miniwob_env(env_id, client_id, remotes, **_):
 
     if env_id == 'wob.mini.NumberCheckboxes-v0':
         env = SoftmaxClickMouse(env, active_region=(10 + 14, 75 + 57, 24 + 55, 132 + 102 + 11 + 22), noclick_regions=[(24 + 11 + 2, 42, 132 + 102 + 9, 28)], discrete_mouse_step=17)
+    elif (env_id == 'wob.mini.BisectAngle-v0') or (env_id == 'wob.mini.FindMidpoint-v0') or (env_id == 'wob.mini.CircleCenter-v0'):
+        env = SoftmaxClickMouseClickAndSubmit(env, active_region=(10, 75 + 50, 10 + 160, 75 + 210 - 35), discrete_mouse_step=8)
     else:
         env = SoftmaxClickMouse(env, discrete_mouse_step=8)
     env = EpisodeID(env)
@@ -328,3 +331,178 @@ class WobRescale(vectorized.ObservationWrapper):
 
     def _observation(self, observation_n):
         return [_process_frame_wob(observation, self.obs_height, self.obs_width) for observation in observation_n]
+
+
+def slither_vnc(space=False, left=False, right=False):
+    return [spaces.KeyEvent.by_name('space', down=space),
+            spaces.KeyEvent.by_name('left', down=left),
+            spaces.KeyEvent.by_name('right', down=right)]
+
+def racing_vnc(up=False, left=False, right=False):
+    return [spaces.KeyEvent.by_name('up', down=up),
+            spaces.KeyEvent.by_name('left', down=left),
+            spaces.KeyEvent.by_name('right', down=right)]
+
+def platform_vnc(up=False, left=False, right=False, space=False):
+    return [spaces.KeyEvent.by_name('up', down=up),
+            spaces.KeyEvent.by_name('left', down=left),
+            spaces.KeyEvent.by_name('right', down=right),
+            spaces.KeyEvent.by_name('space', down=space)]
+
+
+
+
+
+class SoftmaxClickMouse(vectorized.ActionWrapper):
+    """
+    Creates a Discrete action space of mouse clicks.
+
+    This wrapper divides the active region into cells and creates an action for
+    each which clicks in the middle of the cell.
+    """
+    def __init__(self, env, active_region=(10, 75 + 50, 10 + 160, 75 + 210), discrete_mouse_step=10, noclick_regions=[]):
+        super(SoftmaxClickMouse, self).__init__(env)
+        logger.info('Using SoftmaxClickMouse with action_region={}, noclick_regions={}'.format(active_region, noclick_regions))
+        xlow, ylow, xhigh, yhigh = active_region
+        xs = range(xlow, xhigh, discrete_mouse_step)
+        ys = range(ylow, yhigh, discrete_mouse_step)
+        self.active_region = active_region
+        self.discrete_mouse_step = discrete_mouse_step
+        self.noclick_regions = noclick_regions
+        self._points = []
+        removed = 0
+        for x in xs:
+            for y in ys:
+                xc = min(x+int(discrete_mouse_step/2), xhigh-1) # click to center of a cell
+                yc = min(y+int(discrete_mouse_step/2), yhigh-1)
+                if any(self.is_contained((xc, yc), r) for r in noclick_regions):
+                    removed += 1
+                    continue
+                self._points.append((xc, yc))
+        logger.info('SoftmaxClickMouse noclick regions removed {} of {} actions'.format(removed, removed + len(self._points)))
+        self.action_space = gym.spaces.Discrete(len(self._points))
+
+    def _action(self, action_n):
+        return [self._discrete_to_action(int(i)) for i in action_n]
+
+    def _discrete_to_action(self, i):
+        xc, yc = self._points[i]
+        return [
+            spaces.PointerEvent(xc, yc, buttonmask=0), # release
+            spaces.PointerEvent(xc, yc, buttonmask=1), # click
+            spaces.PointerEvent(xc, yc, buttonmask=0), # release
+        ]
+
+    def _reverse_action(self, action):
+        xlow, ylow, xhigh, yhigh = self.active_region
+        try:
+            # find first valid mousedown, ignore everything else
+            click_event = next(e for e in action if isinstance(e, spaces.PointerEvent) and e.buttonmask == 1)
+            index = self._action_to_discrete(click_event)
+            if index is None:
+                return np.zeros(len(self._points))
+            else:
+                # return one-hot vector, expected by demo training code
+                # FIXME(jgray): move one-hot translation to separate layer
+                return np.eye(len(self._points))[index]
+        except StopIteration:
+            # no valid mousedowns
+            return np.zeros(len(self._points))
+
+    def _action_to_discrete(self, event):
+        assert isinstance(event, spaces.PointerEvent)
+        x, y = event.x, event.y
+        step = self.discrete_mouse_step
+        xlow, ylow, xhigh, yhigh = self.active_region
+        xc = min((int((x - xlow) / step) * step) + xlow + step / 2, xhigh - 1)
+        yc = min((int((y - ylow) / step) * step) + ylow + step / 2, yhigh - 1)
+        try:
+            return self._points.index((xc, yc))
+        except ValueError:
+            # ignore clicks outside of active region or in noclick regions
+            return None
+
+    @classmethod
+    def is_contained(cls, point, coords):
+        px, py = point
+        x, width, y, height = coords
+        return x <= px <= x + width and y <= py <= y + height
+
+class SoftmaxClickMouseClickAndSubmit(vectorized.ActionWrapper):
+    """
+    Creates a Discrete action space of mouse clicks.
+
+    This wrapper divides the active region into cells and creates an action for
+    each which clicks in the middle of the cell.
+    """
+    def __init__(self, env, active_region=(10, 75 + 50, 10 + 160, 75 + 210), discrete_mouse_step=10, noclick_regions=[]):
+        super(SoftmaxClickMouseClickAndSubmit, self).__init__(env)
+        logger.info('Using SoftmaxClickMouseSpecial with action_region={}, noclick_regions={}'.format(active_region, noclick_regions))
+        xlow, ylow, xhigh, yhigh = active_region
+        xs = range(xlow, xhigh, discrete_mouse_step)
+        ys = range(ylow, yhigh, discrete_mouse_step)
+        self.active_region = active_region
+        self.discrete_mouse_step = discrete_mouse_step
+        self.noclick_regions = noclick_regions
+        self._points = []
+        removed = 0
+        for x in xs:
+            for y in ys:
+                xc = min(x+int(discrete_mouse_step/2), xhigh-1) # click to center of a cell
+                yc = min(y+int(discrete_mouse_step/2), yhigh-1)
+                if any(self.is_contained((xc, yc), r) for r in noclick_regions):
+                    removed += 1
+                    continue
+                self._points.append((xc, yc))
+        logger.info('SoftmaxClickMouse noclick regions removed {} of {} actions'.format(removed, removed + len(self._points)))
+        self.action_space = gym.spaces.Discrete(len(self._points))
+
+    def _action(self, action_n):
+        return [self._discrete_to_action(int(i)) for i in action_n]
+
+    def _discrete_to_action(self, i):
+        xc, yc = self._points[i]
+        return [
+            spaces.PointerEvent(xc, yc, buttonmask=0), # release
+            spaces.PointerEvent(xc, yc, buttonmask=1), # click
+            spaces.PointerEvent(xc, yc, buttonmask=0), # release
+            # Click on Submit Button
+            spaces.PointerEvent(10 + 160/2, 75 + 50 + 160 - 30/2, buttonmask=0),  # release
+            spaces.PointerEvent(10 + 160 / 2, 75 + 50 + 160 - 30 / 2, buttonmask=1),  # click
+            spaces.PointerEvent(10 + 160 / 2, 75 + 50 + 160 - 30 / 2, buttonmask=0),  # release
+        ]
+
+    def _reverse_action(self, action):
+        xlow, ylow, xhigh, yhigh = self.active_region
+        try:
+            # find first valid mousedown, ignore everything else
+            click_event = next(e for e in action if isinstance(e, spaces.PointerEvent) and e.buttonmask == 1)
+            index = self._action_to_discrete(click_event)
+            if index is None:
+                return np.zeros(len(self._points))
+            else:
+                # return one-hot vector, expected by demo training code
+                # FIXME(jgray): move one-hot translation to separate layer
+                return np.eye(len(self._points))[index]
+        except StopIteration:
+            # no valid mousedowns
+            return np.zeros(len(self._points))
+
+    def _action_to_discrete(self, event):
+        assert isinstance(event, spaces.PointerEvent)
+        x, y = event.x, event.y
+        step = self.discrete_mouse_step
+        xlow, ylow, xhigh, yhigh = self.active_region
+        xc = min((int((x - xlow) / step) * step) + xlow + step / 2, xhigh - 1)
+        yc = min((int((y - ylow) / step) * step) + ylow + step / 2, yhigh - 1)
+        try:
+            return self._points.index((xc, yc))
+        except ValueError:
+            # ignore clicks outside of active region or in noclick regions
+            return None
+
+    @classmethod
+    def is_contained(cls, point, coords):
+        px, py = point
+        x, width, y, height = coords
+        return x <= px <= x + width and y <= py <= y + height
