a03e6be975abcd14481daf178fcf1c47b12c6c03
Thomas Ritter <s9thritt@stud.uni-saarland.de>
2018-02-15 16:33:01 +0100

    removed unused methods

--- universe-starter-agent/action_space.py
+++ universe-starter-agent/action_space.py
@@ -12,23 +12,6 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 universe.configure_logging()
 
-def slither_vnc(space=False, left=False, right=False):
-    return [vnc_spaces.KeyEvent.by_name('space', down=space),
-            vnc_spaces.KeyEvent.by_name('left', down=left),
-            vnc_spaces.KeyEvent.by_name('right', down=right)]
-
-def racing_vnc(up=False, left=False, right=False):
-    return [vnc_spaces.KeyEvent.by_name('up', down=up),
-            vnc_spaces.KeyEvent.by_name('left', down=left),
-            vnc_spaces.KeyEvent.by_name('right', down=right)]
-
-def platform_vnc(up=False, left=False, right=False, space=False):
-    return [vnc_spaces.KeyEvent.by_name('up', down=up),
-            vnc_spaces.KeyEvent.by_name('left', down=left),
-            vnc_spaces.KeyEvent.by_name('right', down=right),
-            vnc_spaces.KeyEvent.by_name('space', down=space)]
-
-
 class SoftmaxClickTask(vectorized.ActionWrapper):
     """
     Creates a Discrete action space of mouse clicks.
@@ -72,35 +55,6 @@ class SoftmaxClickTask(vectorized.ActionWrapper):
             vnc_spaces.PointerEvent(xc, yc, buttonmask=1), # click
             vnc_spaces.PointerEvent(xc, yc, buttonmask=0) # release
         ]
-
-    def _reverse_action(self, action):
-        xlow, ylow, xhigh, yhigh = self.active_region
-        try:
-            # find first valid mousedown, ignore everything else
-            click_event = next(e for e in action if isinstance(e, vnc_spaces.PointerEvent) and e.buttonmask == 1)
-            index = self._action_to_discrete(click_event)
-            if index is None:
-                return np.zeros(len(self._points))
-            else:
-                # return one-hot vector, expected by demo training code
-                # FIXME(jgray): move one-hot translation to separate layer
-                return np.eye(len(self._points))[index]
-        except StopIteration:
-            # no valid mousedowns
-            return np.zeros(len(self._points))
-
-    def _action_to_discrete(self, event):
-        assert isinstance(event, vnc_spaces.PointerEvent)
-        x, y = event.x, event.y
-        step = self.discrete_mouse_step
-        xlow, ylow, xhigh, yhigh = self.active_region
-        xc = min((int((x - xlow) / step) * step) + xlow + step / 2, xhigh - 1)
-        yc = min((int((y - ylow) / step) * step) + ylow + step / 2, yhigh - 1)
-        try:
-            return self._points.index((xc, yc))
-        except ValueError:
-            # ignore clicks outside of active region or in noclick regions
-            return None
 
     @classmethod
     def is_contained(cls, point, coords):
